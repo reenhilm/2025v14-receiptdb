baaa2907522169b5dd18fb2c18d35bd99caa5b3c
Christian Rönnholm <christian@kajal.se>
2025-04-07 16:58:51 +0200

    text min-clamp

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main className="flex h-screen w-full border-2">
       <div className="flex m-auto">
         <div className="flex flex-col items-center gap-15 m-auto">
-          <h1 className="text-2xl sm:text-5xl text-balance text-center">Receipt Hoarding, but Make It Organized</h1>
-          <p className="text-base sm:text-xl text-balance text-center">This app is for my family&apos;s personal use, demo purposes, and impressing recruiters. (Hi, future boss!)</p>
+          <h1 className="text-[min(4.8vw,48px)] text-balance text-center">Receipt Hoarding, but Make It Organized</h1>
+          <p className="text-[min(3.1vw,20px)] text-balance text-center">This app is for my family&apos;s personal use, demo purposes, and impressing recruiters. (Hi, future boss!)</p>
         </div>
       </div>
     </main>
