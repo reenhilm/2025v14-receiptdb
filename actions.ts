fcb43e78c3012a94ec9f078c32beba038dc945be
Christian Rönnholm <christian@kajal.se>
2025-04-14 11:30:16 +0200

    add actions/fetchReceiptById, add api-error class, add interface boilerplate, add constants for common strings

---
+++ actions.ts
@@ -0,0 +1,29 @@
+"use server";
+import { fetchFailedMessage, nextworkMessage } from "./constants";
+import ApiError from "./classes/api-error";
+import { Receipt, Receipts } from "./interfaces/receipt";
+
+export const fetchReceiptById = async (receiptId: string, groupId:number): Promise<Receipt | ApiError> => {
+    try {
+        const res = await fetch(`https://reenhilm.github.io/Data/receiptdb_group${groupId}.json`);
+
+        if (res.status == 404)
+            return ApiError.fromError(404);
+
+        if (!res.ok) {
+            //logging could be done here
+            throw ApiError.fromError(res.status, fetchFailedMessage);
+        }
+
+        const data: Receipts = await res.json();
+        const foundReceipt: Receipt | undefined = data.receipts.find(r => r.id === receiptId);
+        if(foundReceipt === undefined)
+            return ApiError.fromError(404);
+        
+        return foundReceipt;
+
+    } catch {
+        //catching all errors, we don't want to show all internal error-messages to client so providing general error to client
+        return ApiError.fromError(500, nextworkMessage);
+    }
+};
---
+++ interfaces/receipt.ts
@@ -0,0 +1,8 @@
+export interface Receipt {
+    id: string;
+    //TODO add more properties
+}
+
+export interface Receipts {
+    receipts: Receipt[]
+}
