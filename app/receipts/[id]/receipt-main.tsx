584bc8ff68d4c45a64f703521720af9d15256397
Christian Rönnholm <christian@kajal.se>
2025-04-15 16:41:33 +0200

    add more properties to receipt

--- app/receipts/[id]/receipt-main.tsx
+++ app/receipts/[id]/receipt-main.tsx
@@ -2,8 +2,21 @@ import { Receipt } from "@/interfaces/receipt";
 
 export default function ReceiptMain({ receipt }: { receipt: Receipt }) {
     return (
-        <main className="m-auto max-w-120 my-13">
-            {receipt.id}
-        </main>
+        <>
+            <main className="m-auto max-w-120 my-13">
+                {receipt.id}
+                {receipt.retailer_name}
+                {receipt.title}
+                {receipt.description}
+                {receipt.price_total}
+                {receipt.image_url}
+                {receipt.date.purchased}
+                {receipt.date.uploaded}
+                {receipt.category.id}
+                {receipt.category.name}
+                {receipt.uploader.id}
+                {receipt.uploader.fullname}
+            </main>
+        </>
     )
 }
--- interfaces/receipt.ts
+++ interfaces/receipt.ts
@@ -1,4 +1,26 @@
 export interface Receipt {
     id: string;
-    //TODO add more properties
+    retailer_name: string;
+    title: string;
+    description: string;
+    price_total: number;
+    image_url: string;
+    date: ReceiptDates;
+    category: Category;
+    uploader: Uploader;
+}
+
+export interface ReceiptDates {
+    purchased: string;
+    uploaded: string;
+}
+
+export interface Category {
+    id: string;
+    name: string;
+}
+
+export interface Uploader {
+    id: string;
+    fullname: string;
 }
